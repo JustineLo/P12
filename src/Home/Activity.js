8fdffd4226cc60ee8b6479799b067489ecd1ad1d
Justine Lo <juustine.lo@gmail.com>
2023-01-21 18:51:08 +0100

    Activity and count files

---
+++ src/Home/Activity.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+import styles from "./Activity.module.css";
+
+const Activity = () => {
+  return (
+    <>
+      <Header name={user.firstName} />
+    </>
+  );
+};
+
+export default Activity;
---
+++ src/Home/Count.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Count.module.css";
+
+const Count = ({ icon, color, name, unit }) => {
+  return <div className="count"></div>;
+};
+
+export default Count;
