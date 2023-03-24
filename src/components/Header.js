38baaae7ef9cd87ba0cc20f1a4a9c41535c824f9
Justine Lo <juustine.lo@gmail.com>
2023-03-24 17:17:53 +0100

    iden Headercontainer

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,34 @@
 import React from "react";
-import styles from "./Header.module.css";
+import styled from "styled-components";
+
+const HeaderContainer = styled.header`
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+
+  h1 {
+    font-weight: 600;
+    font-size: 48px;
+  }
+
+  h3 {
+    font-weight: 600;
+    font-size: 18px;
+  }
+
+  span {
+    color: var(--primary);
+  }
+`;
 
 const Header = ({ name }) => {
   return (
-    <header className={styles.header}>
+    <HeaderContainer>
       <h1>
-        Bonjour <span className={styles.name}>{name}</span>{" "}
+        Bonjour <span>{name}</span>
       </h1>
       <h3>Félicitation ! Vous avez explosé vos objectifs hier 👏</h3>
-    </header>
+    </HeaderContainer>
   );
 };
 
