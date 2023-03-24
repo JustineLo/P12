a43e4f95d2aa51ced5175db5f1bbef58d8586f05
Justine Lo <juustine.lo@gmail.com>
2023-03-24 17:44:16 +0100

    idem Navbar

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -1,21 +1,42 @@
 import React from "react";
-import styles from "./Navbar.module.css";
+import styled from "styled-components";
 import { ReactComponent as Logo } from "../assets/logo.svg";
+
+const Container = styled.div`
+  height: 90px;
+  background-color: black;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 87px 0 28px;
+  gap: 49px;
+`;
+
+const Navlinks = styled.div`
+  display: flex;
+  gap: 126px;
+
+  a {
+    color: white;
+    text-decoration: none;
+    font-size: 24px;
+  }
+`;
 
 const Navbar = () => {
   const navlinks = ["Accueil", "Profil", "Réglages", "Communauté"];
   return (
     <>
-      <nav className={styles.navbar}>
+      <Container>
         <Logo alt="logo" />
-        <div className={styles.navlinks}>
+        <Navlinks>
           {navlinks.map((link) => (
             <a href="#" key={link}>
               {link}
             </a>
           ))}
-        </div>
-      </nav>
+        </Navlinks>
+      </Container>
     </>
   );
 };
